4f2eab4da9a29564c85482921f297e44dc74b6e3
yz <cookie.yz@qq.com>
2017-10-23 17:18:11 +0800

    update weblogwash

--- weblogwash/src/mr/weblogwash/WebLogParser.java
+++ weblogwash/src/mr/weblogwash/WebLogParser.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.Locale;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
+import java.util.Set;
 
 public class WebLogParser {
 	
@@ -24,8 +24,13 @@ public class WebLogParser {
 			bean.setBody_bytes_sent(arr[9]);
 			bean.setHttp_referer(arr[10]);
 			
+			//如果useragent元素较多，拼接useragent
 			if (arr.length > 12) {
-				bean.setHttp_user_agent(arr[11] + " " + arr[12]);
+				StringBuilder sb = new StringBuilder();
+				for(int i = 11; i < arr.length; i++){
+					sb.append(arr[i]);
+				}
+				bean.setHttp_user_agent(sb.toString());
 			} else {
 				bean.setHttp_user_agent(arr[11]);
 			}
@@ -34,11 +39,21 @@ public class WebLogParser {
 				bean.setValid(false);
 			}
 			
+			if("-invalid_time-".equals(webLogBean.getTime_local())){
+				webLogBean.setValid(false);
+			}
+			
 		} else {
 			bean.setValid(false);
 		}
 		
 		return bean;
+	}
+	
+	public static void filtStaticResource(WebLogBean bean, Set<String> pages) {
+		if (!pages.contains(bean.getRequest())) {
+			bean.setValid(false);
+		}
 	}
 	
 	static public String parseTime(String dt) {
--- weblogwash/src/mr/weblogwash/WebLogPreProcess.java
+++ weblogwash/src/mr/weblogwash/WebLogPreProcess.java
@@ -1,6 +1,8 @@
 package mr.weblogwash;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -12,20 +14,48 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+/**
+ * 处理原始日志，过滤出真实pv请求
+ * 转换时间格式
+ * 对缺失字段填充默认值
+ * 对记录标记valid和invalid
+ * 
+ * @author
+ *
+ */
+
 public class WebLogPreProcess {
 
 	static class WebLogPreProcessMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
+		//用来存储网站url分类数据
+		Set<String> pages = new HashSet<String>();
 		private Text k = new Text();
 		private NullWritable v = NullWritable.get();
+		
+		/**
+		 * 从外部加载网站url分类数据
+		 */
+		@Override
+		protected void setup(Context context) throws IOException, InterruptedException {
+			pages.add("/about");
+			pages.add("/black-ip-list/");
+			pages.add("/cassandra-clustor/");
+			pages.add("/finance-rhive-repurchase/");
+			pages.add("/hadoop-family-roadmap/");
+			pages.add("/hadoop-hive-intro/");
+			pages.add("/hadoop-zookeeper-intro/");
+			pages.add("/hadoop-mahout-roadmap/");
+		}
 		
 		@Override
 		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
 			WebLogBean bean = WebLogParser.parser(line);
 			
-			if (!bean.isValid()) {
-				return;
-			}
+			// 过滤js/图片/css等静态资源 
+			WebLogParser.filtStaticResource(webLogBean, pages);
+			
+			/*if (!bean.isValid()) { return; }*/
 			
 			k.set(bean.toString());
 			context.write(k, v);
